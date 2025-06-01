6938f8ad588cb1a5ee42b0f24c555a5378032c40
Francky Andriamampionona <andriafrancky3@gmail.com>
2025-06-01 22:22:48 +0200

    Add files via upload

---
+++ RandomNormalGenerator.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include<random>
+#include <corecrt_math_defines.h>
+#include <cmath>
+
+class StandardGaussianGenerator {
+public:
+    // Constructor
+    StandardGaussianGenerator()
+        : _eng(_rd()), _dist(0., 1.) {}
+
+    // Generate a random number following the standard normal distribution
+    double generate() const {
+        return _dist(_eng);
+    }
+
+private:
+    // Non-deterministic random number generator
+    std::random_device _rd;
+
+    // Mersenne Twister pseudo-random generator
+    mutable std::mt19937 _eng;
+
+    // Normal distribution
+    mutable std::normal_distribution<double> _dist;
+};
